22a5113dee2eb8ab8158a8690d337bcd45be7fa0
shio8613 <psp1070@gmail.com>
2022-12-21 09:57:01 +0900

    検索機能実装

--- src/main/java/com/github/shiro8613/advancedlogviewer/SearchWindowController.java
+++ src/main/java/com/github/shiro8613/advancedlogviewer/SearchWindowController.java
@@ -1,2 +1,36 @@
-package com.github.shiro8613.advancedlogviewer;public class SearchWindowController {
+package com.github.shiro8613.advancedlogviewer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class SearchWindowController {
+
+    @FXML
+    private Button Cancelbutton;
+
+    private boolean Canceled = false;
+    @FXML
+    private TextField textfield;
+
+
+    private String text;
+    public String getText() { return this.text; }
+    public boolean isCanceled() { return this.Canceled; }
+    @FXML
+    protected void CancelButton() {
+        Stage stage = (Stage) Cancelbutton.getScene().getWindow();
+        this.Canceled = true;
+        stage.close();
+    }
+
+    @FXML
+    protected void ExecuteButton() {
+        Stage stage = (Stage) Cancelbutton.getScene().getWindow();
+        this.text = textfield.getText();
+
+        stage.close();
+    }
+
 }
