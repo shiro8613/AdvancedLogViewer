5aab5526fd0109b2bd65bade184691c81e05c855
shio8613 <psp1070@gmail.com>
2022-12-21 13:02:25 +0900

    処理がおかしいのでいったん削除

--- src/main/java/com/github/shiro8613/advancedlogviewer/logic/filter/View.java
+++ src/main/java/com/github/shiro8613/advancedlogviewer/logic/filter/View.java
@@ -3,24 +3,14 @@ package com.github.shiro8613.advancedlogviewer.logic.filter;
 import javafx.scene.control.Label;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 public class View {
 
     public List<Label> Marking(List<Label> Old, List<Label> New, String color) {
-        List<Label> list = new ArrayList<Label>();
-        Old.forEach(x -> {
-            New.forEach(y -> {
-                if (x == y) {
-                    y.setStyle("-fx-background-color: "+color+";");
-                    list.add(y);
-                } else {
-                    list.add(x);
-                }
-            });
-        });
-
-        return list;
+        return new ArrayList<>();
     }
 
 }
