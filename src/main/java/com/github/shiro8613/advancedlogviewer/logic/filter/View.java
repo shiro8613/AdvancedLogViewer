2e6f2778a445f422c3e59b79f195868ea08bc22d
shio8613 <psp1070@gmail.com>
2022-12-23 10:46:52 +0900

    修正

--- src/main/java/com/github/shiro8613/advancedlogviewer/logic/filter/View.java
+++ src/main/java/com/github/shiro8613/advancedlogviewer/logic/filter/View.java
@@ -3,14 +3,14 @@ package com.github.shiro8613.advancedlogviewer.logic.filter;
 import javafx.scene.control.Label;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.stream.Stream;
 
 public class View {
 
-    public List<Label> Marking(List<Label> Old, List<Label> New, String color) {
+    public List<Label> Marking(Stream Data, List<Label> New, String color) {
         List<Label> list = new ArrayList<>();
+        List<Label> Old = (List<Label>) Data;
         List<Integer> integers = new ArrayList<>();
 
         Old.forEach(x -> {
